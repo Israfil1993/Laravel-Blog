9434a12e32086c80500ea0bc9f476cdc5431a826
Israfil Rahimbayli <israfil.rahimbeyli@gmail.com>
2023-04-29 01:29:13 +0400

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.frontend.master')
+@section('title','Milli Portal | Ana Səhifə')
+@section('content')
+		@include('frontend.includes.content')
+        @include('frontend.includes.sidebar')
+@endsection
+
---
+++ resources/views/layouts/frontend/master.blade.php
@@ -0,0 +1,12 @@
+@include('frontend.includes.header')
+
+@yield('content')
+</div>
+</div>
+</div>
+
+
+@include('frontend.includes.subscribe')
+
+
+@include('frontend.includes.footer')
